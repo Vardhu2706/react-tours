39ddb698d0463f2a417bd1dae965ce5453515942
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-04-29 06:02:22 +0530

    Added functionality to Tours & Single Tour components.

--- src/Tour.js
+++ src/Tour.js
@@ -2,8 +2,20 @@
 import React, { useState } from "react";
 
 // Functional Component
-const Tour = () => {
-  return <h2>Tour Component</h2>;
+const Tour = ({ id, image, info, price, name }) => {
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>{info}</p>
+        <button className="delete-btn">Not Interested</button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
