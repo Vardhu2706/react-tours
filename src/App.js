e0fa7439bc596da0a006b7b99147fad83f26143f
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-04-29 05:54:06 +0530

    Fetched data from Api & set up conditional rendering.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 // Importing Helpers
-import React, { useState, userEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 // Importing Components/Files
 import Loading from "./Loading";
 import Tours from "./Tours";
 
 const url = "https://course-api.com/react-tours-project";
+
+// Functional Component
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  // State variables
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  // useEffect hook
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  // Conditional Rendering
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
+// Default Export
 export default App;
